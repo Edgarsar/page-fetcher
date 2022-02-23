bafde15b6d0899f09bc85c9437f859188d5ec261
Edgarsar <edgarsargsyan@hotmail.com>
2022-02-23 22:35:06 +0000

    page downloader app

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+
+const fs = require('fs');
+
+
+const url = process.argv[2];
+const localPath = process.argv[3];
+
+request(url, (error, response, body) => {
+  fs.writeFile(localPath, body, err => {
+
+    if (err) {
+      console.error(err);
+    }
+    else {
+
+      console.log(`Downloaded and saved ${body.length} bytes to ${localPath}`);
+    }
+
+    return;
+  });
+
+});
+
+
+
+
+
+
